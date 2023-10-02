8a0455a11dd5c84493f296ec61c03af154c0828e
Ycaro Rodrigues <ycaro.rodrigues@spread.com.br>
2023-10-02 11:20:21 -0300

    AboutActivity documentada

--- app/src/main/java/com/example/countriesproject/AboutActivity.kt
+++ app/src/main/java/com/example/countriesproject/AboutActivity.kt
@@ -5,24 +5,28 @@ import android.text.method.LinkMovementMethod
 import androidx.appcompat.app.AppCompatActivity
 import com.example.countriesproject.databinding.ActivityAboutBinding
 
+/*
+* Esta classe representa a Activity que exibe informações sobre o aplicativo
+* */
 class AboutActivity : AppCompatActivity() {
 
+    //Binding para a interface de usuário
     private lateinit var binding: ActivityAboutBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //Infla o layout e configura a view
         binding = ActivityAboutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Configuração da ActionBar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Configura os links para abrir no navegador
         binding.linkCopyrightFlags.movementMethod = LinkMovementMethod.getInstance()
         binding.linkOtherImages.movementMethod = LinkMovementMethod.getInstance()
         binding.linkCopyrightImage.movementMethod = LinkMovementMethod.getInstance()
-
-
-
-
     }
 }
